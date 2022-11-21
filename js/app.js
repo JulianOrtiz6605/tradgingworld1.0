15eb2888e753c9ab2d381d783a91bf70dcbf1e96
Julián David <juliandavid06g@gmail.com>
2022-11-21 07:07:34 -0500

    Cambié la cantidad de puntos estelares a 45

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ particlesJS(
     {
         "particles": {
           "number": {
-            "value": 50,
+            "value": 45,
             "density": {
               "enable": true,
               "value_area": 800
